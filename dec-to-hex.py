2c5f1901c0ffa2c6a8c1edf10f95a9b5495b9c12
scv_ready <74455344+portalbay@users.noreply.github.com>
2022-04-22 03:10:29 -0400

    Create dec-to-hex.py

---
+++ dec-to-hex.py
@@ -0,0 +1,22 @@
+!/usr/bin/python
+
+from sys import argv
+
+oid = argv[1]
+
+oid = oid.split('.')
+
+oid.pop(0)
+oid.pop(0)
+
+mac = ""
+for i in oid:
+    temp = str(hex(int(i))).replace("0x","")
+
+    if len(temp) ==1:
+        temp = "0" + temp
+
+    mac = mac +":"+ temp
+
+mac = mac[1:]
+print(mac)
